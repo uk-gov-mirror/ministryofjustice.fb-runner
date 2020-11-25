822ef1f15d9b3a27cfba0b49c65f06dac5eb0dd2
matttei <matt.tei@digital.justice.gov.uk>
2020-11-25 14:42:01 +0000

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-	def service_metadata	
-    Rails.configuration.service_metadata	
+  def service
+    @service ||= MetadataPresenter::Service.new(service_metadata)
+  end
+
+	def service_metadata
+    Rails.configuration.service_metadata
+  end
+
+  def save_user_data
+  end
+
+  def load_user_data
   end
 end
